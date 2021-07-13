7a74981300f0a20fcb1653de569a405e93edb594
DaviD D <jdavid.martinez@gmail.com>
2021-07-12 21:10:16 -0500

    Secction  19 video 251 - Refresher: finished

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,41 +1,38 @@
-import { createSlice } from "@reduxjs/toolkit";
-
+import { createSlice } from '@reduxjs/toolkit';
 
 const cartSlice = createSlice({
-  name: "cart",
+  name: 'cart',
   initialState: {
     items: [],
-    totalQuantuty: 0,
-    totalAmount: 0,
+    totalQuantity: 0,
   },
   reducers: {
     addItemToCart(state, action) {
       const newItem = action.payload;
-      const existingItem = state.items.findIndex(
-        (item) => item.id === newItem.id
-      );
+      const existingItem = state.items.find((item) => item.id === newItem.id);
+      state.totalQuantity++;
       if (!existingItem) {
-        state.item.push({
-          itemId: newItem.id,
+        state.items.push({
+          id: newItem.id,
           price: newItem.price,
           quantity: 1,
           totalPrice: newItem.price,
-          name: newItem.title,
+          name: newItem.title
         });
-      }else {
-          existingItem.quantity++;
-          existingItem.totalPrice = existingItem.totalPrice + newItem.price;
+      } else {
+        existingItem.quantity++;
+        existingItem.totalPrice = existingItem.totalPrice + newItem.price;
       }
     },
     removeItemFromCart(state, action) {
-        const id = action.payload;
-        const existingItem = state.item.find(item => item.id === id);
-        if(existingItem.quantity === 1){
-            state.items = state.items.filter( item => item.id !== id )
-        }else{
-            existingItem.quantity--;
-            existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
-        }
+      const id = action.payload;
+      const existingItem = state.items.find(item => item.id === id);
+      state.totalQuantity--;
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter(item => item.id !== id);
+      } else {
+        existingItem.quantity--;
+      }
     },
   },
 });
@@ -43,3 +40,4 @@ const cartSlice = createSlice({
 export const cartActions = cartSlice.actions;
 
 export default cartSlice;
+
