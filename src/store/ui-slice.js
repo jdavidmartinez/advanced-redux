631aa9859e08e9c9da33facb67aaac83877cc6ce
DaviD D <jdavid.martinez@gmail.com>
2021-07-16 16:32:21 -0500

    Secction  19 video 257 - Handling Http States & feedbacks with Redux

--- src/store/ui-slice.js
+++ src/store/ui-slice.js
@@ -4,9 +4,20 @@ const uiSlice = createSlice({
   name: "ui",
   initialState: {
     cartIsVisible: false,
+    notification: null,
   },
   reducers: {
-    toggle(state) { state.cartIsVisible = !state.cartIsVisible },
+    toggle(state) {
+      state.cartIsVisible = !state.cartIsVisible;
+    },
+
+    showNotification(state, action) {
+      state.notification = {
+        status: action.payload.status,
+        title: action.payload.title,
+        message: action.payload.message,
+      };
+    },
   },
 });
 export const uiActions = uiSlice.actions;
